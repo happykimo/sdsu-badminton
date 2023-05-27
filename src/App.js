d9dd65e9013a281f83ec8f0a93d1bb84837c2444
Kimo <happykimo@hotmail.com>
2023-05-26 17:23:42 -0700

    sdsu-badminton:added files from my local to the repo

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home} />
+          <Route path='/products' exact Component={Products} />
+          <Route path='/services' exact Component={Services} />
+          <Route path='/sign-up' exact Component={SignUp}/>
+        </Routes>
+      </Router>
+    </>
+    
+  );
+}
+
+export default App;
