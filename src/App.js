f954bb6e00b5b84dfca4c800516daa60331981d1
Kimo <happykimo@hotmail.com>
2023-06-11 21:53:39 -0700

    sdsubadminton:changes a bunch of files

--- src/App.js
+++ src/App.js
@@ -24,4 +24,18 @@ function App() {
   );
 }
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.CardItem');
+hiddenElements.forEach((el) => observer.observe(el));
+
 export default App;
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -8,7 +8,7 @@ function CardItem(props) {
             <Link className='cards__item__link' to={props.path}>
                 <figure className='cards__item__pic-wrap' 
                 data-category={props.label}>
-                    <img src={props.src} alt='Travel'
+                    <img src={props.src} alt='card item'
                     className='cards__item__img'/>
                 </figure>
                 <div className='cards__item__info'>
